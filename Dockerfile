8f283fc0e4da9ac8b33192dda439620bb4cca26d
Nick <nick@nine.ch>
2018-09-03 17:09:10 +0200

    prepare to use the example on openshift

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM php:7.2-apache
 
+ENV MYSQL_HOST=mysql \
+    MYSQL_USER=workshop \
+    MYSQL_DATABASE=workshop
+
+RUN docker-php-ext-install mysqli
+
+RUN chmod g+w /var/run/apache2 && \
+    chgrp root /var/run/apache2
+
 RUN rm /etc/apache2/sites-enabled/*
 
 COPY files/default /etc/apache2/sites-enabled/default.conf
--- files/index.php
+++ files/index.php
@@ -10,5 +10,30 @@
 
   <h1>Hello World, have a great <?= date("l") ?>!</h1>
 
+  <?php
+    if (getenv("MYSQL_HOST") == "") {
+      die("Please provide the MYSQL_HOST environment variable, so I know where I have to connect to.");
+    }
+
+    $mysqli = new mysqli(getenv("MYSQL_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+    if ($mysqli->connect_errno) {
+      die("Database connection failed: " . $mysqli->connect_error);
+    }
+  ?>
+
+  <p>Connection to mysql established.</p>
+
+  <?php
+    $statement = $mysqli->prepare("SELECT VERSION() as version;");
+    $statement->execute();
+    $result = $statement->get_result();
+
+    $row = $result->fetch_assoc();
+  ?>
+
+  <p>MySQL version: <?= $row["version"] ?></p>
+
+  <h2>Environment variables:</h2>
+  <pre><?php print_r($_ENV); ?></pre>
 </body>
 </html>
